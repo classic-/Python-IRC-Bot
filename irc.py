04eaa0f254e5adbaf58e512c198ed30e18319844
Kyle K <kyliek24@hotmail.co.uk>
2012-04-22 03:57:32 +0100

    cleaned up a lot of things, laid foundations of the architecture

--- irc.py
+++ irc.py
@@ -4,18 +4,52 @@ import json
 class IRCConfig:
     def __init__(self, cfgname):
         self._rawcfg = json.loads(open(cfgname).read())
+
     def get_user(self):
         return self._rawcfg['user']
+
     def get_nick(self):
         return self._rawcfg['nick']
+
     def get_servers(self):
         return self._rawcfg['servers']
 
-class IRCBot:
-    def set_config(self, cfg):
-        self._config = cfg
-        self._socket = socket.socket()
-    def connect(self):
-        # TODO
-        self._socket.close()
 
+class IRCParser:
+    def parse_normal(self, line):
+        """Parse line as a normal IRC message"""
+        return
+
+    def parse_misc(self, line):
+        """Parse line as a miscellaneous message"""
+        return
+
+    def parse(self, line):
+        """Parse line according to the IRC specification"""
+        return
+
+
+class IRCManager:
+    def __init__(self, cfg):
+        self._config = cfg
+        self._bots = []
+    
+    def init(self, rejoin):
+        """Initiates the manager.
+
+        Bots attempt to reconnect after rejoin seconds
+        """
+        # TODO: read server list in _cfg, spawn bot for each server
+        return
+
+
+class IRCBot:
+    def __init__(self, nick, user):
+        self._socket = socket.socket()
+        self._parser = IRCParser()
+
+    def connect(self, host, port):
+        self._socket.connect(host, port)
+        # TODO: Read, Parse, React
+        self._socket.close()
+    
