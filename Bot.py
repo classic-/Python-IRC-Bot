74e37afe48f141fe7121cc318cc9ae120095a97f
Pure_ <pure2@hotmail.co.uk>
2012-04-22 03:15:45 +0100

    Added Main and Bot classes

---
+++ Bot.py
@@ -0,0 +1,24 @@
+'''
+Created on 22 Apr 2012
+
+@author: Pure_
+'''
+
+import socket
+
+class Bot(object):
+    
+    def __init__(self, hostname, port, socket_buffer, user, nick, channels):
+        bot = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        bot.connect(hostname, port)
+        
+        bot.send('USER ' + user)
+        bot.send('NICK ' + nick)
+        
+        for channel in channels:
+            bot.send('JOIN ' + channel)
+        
+        while True:
+            s = bot.recv(socket_buffer)
+            
+            
---
+++ Main.py
@@ -0,0 +1,15 @@
+'''
+Created on 22 Apr 2012
+
+@author: Pure_
+'''
+
+import IRCConfig;
+import Bot;
+
+class Main(object):
+
+    def __init__(self, selfparams):
+        # config = IRCConfig('irc.json')
+        # bot = Bot(self, hostname, port, 1024, config.getUser(), config.getNick(), config.getChannels())
+        # Here we will also have to set up multiple 'Bot' objects depending on how many servers we have to connect to
